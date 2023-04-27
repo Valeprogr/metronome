778fcbea0f8a3ed9a68ac4f33de66ac8b431049c
valentina vittoria <valeviit@icloud.com>
2023-04-28 00:16:48 +0200

    add audio and create function for change bpm

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import "./metronome.css"
+import React,{useState} from 'react';
+import "./metronome.css";
+import click1 from "./click1.wav";
 
+let timer = null;
 const Metronome = () => {
-    let bmp = 100;
-    let play = false;
+
+    const [play, setPlay] = useState(false);
+    const [bpm, setBpm] = useState(100);
+
+
+    const handleBpmChange = (event) => {
+        const bpm = event.target.value;
+        setBpm(bpm);
+
+        if (play) {
+            // Stop the old timer and start a new one
+            clearInterval(timer);
+            timer = setInterval(playClick, (60 / bpm) * 1000);
+         
+            // Set the new BPM
+            setBpm(bpm);
+          } else {
+            // Otherwise just update the BPM
+            setBpm(bpm);
+          }
+    }
+    //Audio File in a const
+    const audioClick1 = new Audio(click1);
+    //Function PlayAudio
+    const playClick = () => {
+        audioClick1.play();
+     };
+    const startStop = () => {
+        if (play) {
+            //Stop timer
+            clearInterval(timer);
+            setPlay(false);
+        } else {
+            //Start a timer with the current BPM
+            timer = setInterval(playClick, (60 / bpm) * 1000);
+            setPlay(true);
+            playClick();
+        }
+        audioClick1.play();
+    }
+
 
     return (
         <div className='metronome'>
             <div className='bpm-slider'>
-                <div>{bmp} BPM</div>
-                <input type='range' min="60" max="260" value={bmp} />
+                <div>{bpm} BPM</div>
+                <input
+                    type='range'
+                    min="60"
+                    max="240"
+                    value={bpm}
+                onChange={handleBpmChange}/>
             </div>
-            <button>{play ? 'Stop' : 'Start'}</button>
+            <button onClick={startStop}>{play ? 'Stop' : 'Start'}</button>
         </div>
     );
 }
